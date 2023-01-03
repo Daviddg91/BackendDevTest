3958899bb5b4250072ead0afae9585a7c05d71dc
David <46385259+Daviddg91@users.noreply.github.com>
2023-01-03 01:18:16 +0100

    modified:   README.md 	modified:   docker-compose.yaml 	deleted:    existingApis.yaml 	modified:   mockHystrixRunaway/docker-compose.yml 	new file:   mockHystrixRunaway/logs/archived/debug.2023-01-02.0.log 	new file:   mockHystrixRunaway/logs/archived/debug.2023-01-03.0.log 	new file:   mockHystrixRunaway/logs/archived/debug.2023-01-03.1.log 	new file:   mockHystrixRunaway/logs/archived/debug.2023-01-03.2.log 	new file:   mockHystrixRunaway/logs/archived/debug.2023-01-03.3.log 	modified:   mockHystrixRunaway/logs/movie-catalog-service.log 	modified:   mockHystrixRunaway/src/main/java/services/SimilarProductService.java 	modified:   mockHystrixRunaway/src/main/resources/application.yml 	deleted:    similarProducts.yaml 	new file:   springapp/.project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# BackendDevTest
+# BackendDevTest: Hystrix Mock RunAway1.0
+This app is dockerized in the general docker-compose.yaml but also can import project maven in IDE.
+
+#Installation:
+
+PathRoot-> docker-compose up
+
+
+#Information:
+The results of mocks is cached in app by springboot.
+when any request is wrong in the console of containers output the message "error function fallback"
+
+
--- mockHystrixRunaway/src/main/java/services/SimilarProductService.java
+++ mockHystrixRunaway/src/main/java/services/SimilarProductService.java
@@ -16,21 +16,23 @@ public class SimilarProductService {
     @Autowired
     private SimilarCalls similarsCalls;
     
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem",commandProperties = {
+    @HystrixCommand(fallbackMethod = "getFallback",commandProperties = {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),
             @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "50"),
             @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "5000"),
-//			@HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_STRATEGY, value = "THREAD"),
+			@HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_STRATEGY, value = "SEMAPHORE"),
 //          @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS, value = "3000"),
 //          @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_INTERRUPT_ON_TIMEOUT, value = "true"),
           @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_TIMEOUT_ENABLED, value = "false"),
+          @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_SEMAPHORE_MAX_CONCURRENT_REQUESTS,value = "280"),
 
             },
             threadPoolKey = "similarInfoPool",
             threadPoolProperties = {
-                    @HystrixProperty(name = "coreSize",value = "2"),
-                    @HystrixProperty(name = "maxQueueSize",value = "200"),
+                    @HystrixProperty(name = "coreSize",value = "560"),
+                    @HystrixProperty(name = "maxQueueSize",value = "-1"),
+//                    @HystrixProperty(name = "allowMaximumSizeToDivergeFromCoreSize", value = "true")
 
             })
 	public SimilarProducts getSimilarProducts(Long productId) {
@@ -45,9 +47,9 @@ public class SimilarProductService {
     
     
 
-    public SimilarProducts getFallbackCatalogItem(Long productId) {
+    public SimilarProducts getFallback(Long productId) {
 //        return new CatalogItem("Movie Not Found","",rating.getRating());
-    	System.out.println("error ");
+    	System.out.println("error function fallback");
 		return new SimilarProducts();
     }
 }
